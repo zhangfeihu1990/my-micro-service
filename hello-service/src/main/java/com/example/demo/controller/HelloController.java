fab279b8e411f43e4c5546c95e556e5de9ebc159
mahui <mahuijoin@sina.com>
2019-03-08 17:59:48 +0800

    rabbitmq

---
+++ hello-service/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Administrator on 2019/3/7.
+ */
+public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass());
+    @Autowired
+    private DiscoveryClient client;
+    @RequestMapping(value= "/hello", method= RequestMethod.GET)
+    public String index() {
+        ServiceInstance instance = client.getLocalServiceinstance();
+        logger.info("/hello, host:" + instance.getHost()+",service id:" +
+                instance.getServiceid());
+
+        return "Hello World";
+    }
+}
